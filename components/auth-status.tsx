6e4ea772aa79fc53767e2cd19d07b42067b25802
tj2904 <tj2904+github@gmail.com>
2023-07-27 21:54:47 +0100

    fix typo

--- components/auth-status.tsx
+++ components/auth-status.tsx
@@ -3,7 +3,7 @@ import { getServerSession } from "next-auth/next";
 export default async function AuthStatus() {
   const session = await getServerSession();
   return (
-    <div className= mx-auto max-w-7xl px-6 lg:px-8 mt-4 top-5  w-full  flex justify-center items-center pb-4 border-b border-white/10 mb-6 ">
+    <div className="mx-auto max-w-7xl px-6 lg:px-8 mt-4 top-5  w-full  flex justify-center items-center pb-4 border-b border-white/10 mb-6 ">
       {session && (
         <p className=" text-sm">Signed in as {session.user?.email}</p>
       )}
