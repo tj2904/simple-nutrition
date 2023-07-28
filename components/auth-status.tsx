508e38b780caab874a8b766744f18e7d8a2510ee
tj2904 <tj2904+github@gmail.com>
2023-07-28 10:25:51 +0100

    updates styling to match footer ruled line

--- components/auth-status.tsx
+++ components/auth-status.tsx
@@ -3,10 +3,16 @@ import { getServerSession } from "next-auth/next";
 export default async function AuthStatus() {
   const session = await getServerSession();
   return (
-    <div className="mx-auto max-w-7xl px-6 lg:px-8 mt-4 top-5  w-full  flex justify-center items-center pb-4 border-b border-white/10 mb-6 ">
+    <>
       {session && (
-        <p className=" text-sm">Signed in as {session.user?.email}</p>
+        <div className="mx-auto max-w-7xl px-6 pt-6  lg:px-8">
+          <div className="border-b border-white/10 pb-4   ">
+            <div className=" text-sm text-center">
+              Signed in as {session.user?.email}
+            </div>
+          </div>
+        </div>
       )}
-    </div>
+    </>
   );
 }
