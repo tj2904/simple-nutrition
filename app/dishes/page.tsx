82bcc3e1cfe4ab61319957d3ab851c679878a0df
tj2904 <tj2904+github@gmail.com>
2023-08-16 02:05:22 +0100

    adds wrapping to ingredient column removes console.logs updates keys

--- app/dishes/page.tsx
+++ app/dishes/page.tsx
@@ -2,8 +2,8 @@ import DataTable from "@/components/dataTable";
 
 export default function Dishes() {
   return (
-    <>
+    <section className="min-h-screen">
       <DataTable />
-    </>
+    </section>
   );
 }
