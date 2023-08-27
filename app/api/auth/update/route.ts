844ec702bf3d99e32937208d089365b92ca4ba8c
tj2904 <tj2904+github@gmail.com>
2023-08-27 23:20:26 +0100

    adds name functionality

---
+++ app/api/auth/update/route.ts
@@ -0,0 +1,25 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const { email, firstName, surname } = await req.json();
+  const exists = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (exists) {
+    prisma.user.update({
+      where: {
+        email,
+      },
+      data: {
+        firstName,
+        surname,
+      },
+    });
+    return NextResponse.json({ message: "User updated" }, { status: 200 });
+  } else {
+    return NextResponse.json({ error: "User does not exist" }, { status: 400 });
+  }
+}
