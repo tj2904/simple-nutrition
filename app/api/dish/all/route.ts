48d920359c4a3c1f66d955b46d55d7f7f7efcea1
tj2904 <tj2904+github@gmail.com>
2023-09-01 16:49:16 +0100

    roll back api changes

--- app/api/dish/all/route.ts
+++ app/api/dish/all/route.ts
@@ -1,22 +1,18 @@
 import prisma from "@/lib/prisma";
-import { cache } from "react";
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
-export const revalidate = 0;
 export async function GET(req: Request) {
-  const dishesWithIngredients = cache(async (id: string) => {
-    const getDishesWithIngredients = await prisma.dish.findMany({
-      include: {
-        ingredients: {
-          select: {
-            ingredientId: true,
-            name: true,
-          },
+  const dishesWithIngredients = await prisma.dish.findMany({
+    include: {
+      ingredients: {
+        select: {
+          ingredientId: true,
+          name: true,
         },
       },
-    });
-
-    return NextResponse.json(getDishesWithIngredients);
+    },
   });
+
+  return NextResponse.json(dishesWithIngredients);
 }
