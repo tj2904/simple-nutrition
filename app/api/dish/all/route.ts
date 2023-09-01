e2b80b84f5690fb8163d7189f585075bbd185f48
tj2904 <tj2904+github@gmail.com>
2023-09-01 17:00:39 +0100

    use force-dynamic

--- app/api/dish/all/route.ts
+++ app/api/dish/all/route.ts
@@ -1,27 +1,20 @@
 import prisma from "@/lib/prisma";
-import { NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 
-export default async function GET(req: Request, res: NextApiResponse) {
-  try {
-    // Fetch data from your database or source
-    const dishesWithIngredients = await prisma.dish.findMany({
-      include: {
-        ingredients: {
-          select: {
-            ingredientId: true,
-            name: true,
-          },
+export const dynamic = "force-dynamic";
+
+export async function GET(req: Request) {
+  const dishesWithIngredients = await prisma.dish.findMany({
+    include: {
+      ingredients: {
+        select: {
+          ingredientId: true,
+          name: true,
         },
       },
-    });
+    },
+  });
 
-    // Set caching headers to prevent caching
-    res.setHeader("Cache-Control", "no-store, max-age=0");
-
-    // Return the response as JSON
-    res.json(dishesWithIngredients);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+  return NextResponse.json(dishesWithIngredients);
 }
