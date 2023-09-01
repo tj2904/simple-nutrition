80a0fd7dd7488bf256ce0f9c3536ee4cf6cdefde
tj2904 <tj2904+github@gmail.com>
2023-09-01 16:54:45 +0100

    update api

--- app/api/dish/all/route.ts
+++ app/api/dish/all/route.ts
@@ -1,18 +1,27 @@
 import prisma from "@/lib/prisma";
-import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
+import { NextApiResponse } from "next";
 
-export async function GET(req: Request) {
-  const dishesWithIngredients = await prisma.dish.findMany({
-    include: {
-      ingredients: {
-        select: {
-          ingredientId: true,
-          name: true,
+export default async function GET(req: Request, res: NextApiResponse) {
+  try {
+    // Fetch data from your database or source
+    const dishesWithIngredients = await prisma.dish.findMany({
+      include: {
+        ingredients: {
+          select: {
+            ingredientId: true,
+            name: true,
+          },
         },
       },
-    },
-  });
+    });
 
-  return NextResponse.json(dishesWithIngredients);
+    // Set caching headers to prevent caching
+    res.setHeader("Cache-Control", "no-store, max-age=0");
+
+    // Return the response as JSON
+    res.json(dishesWithIngredients);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
